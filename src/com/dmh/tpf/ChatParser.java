16d88ee70223f8c0f9d961667a81d857a1b6c853
Dexter Haslem <dexter.haslem@gmail.com>
2015-11-02 05:25:04 -0700

    more effecient way to handle the extended stuff by skipping w/o copys

--- src/com/dmh/tpf/ChatParser.java
+++ src/com/dmh/tpf/ChatParser.java
@@ -40,6 +40,12 @@ public class ChatParser {
         // oh god, thats all great but we need to handle twitch extensions (IRCv3 stuff)
         //@color=#0D4200;display-name=TWITCH_UserNaME;emotes=25:0-4,12-16/1902:6-10;subscriber=0;turbo=1;user-type=global_mod :twitch_username!twitch_username@twitch_username.tmi.twitch.tv PRIVMSG #channel :Kappa Keepo Kappa
         //@color=#0D4200;display-name=TWITCH_UserNaME;emote-sets=0,33,50,237,793,2126,3517,4578,5569,9400,10337,12239;subscriber=1;turbo=1;user-type=staff :tmi.twitch.tv USERSTATE #channel
+
+        // handle IRCv3 stuff before split to avoid excessive copying and string interning
+        // nasty. skip it and pretend it didnt exist
+        if (raw.charAt(0) == '@')
+            raw = raw.substring(raw.indexOf(' ') + 1);
+
         String[] msgChunks = raw.split(" ");
 
         // we need at least a prefix and command
@@ -47,15 +53,15 @@ public class ChatParser {
             return null;
 
         // turbo fuck hack: if its IRCv3 tagged, drop that chunk
-        if (msgChunks[0].length() > 1 && msgChunks[0].charAt(0) == '@') {
-            String extendedCrap = msgChunks[0];
-            String[] tmp = new String[msgChunks.length - 1];
-            System.arraycopy(msgChunks, 1, tmp, 0, tmp.length);
-            msgChunks = tmp;
+        //if (msgChunks[0].length() > 1 && msgChunks[0].charAt(0) == '@') {
+        //    String extendedCrap = msgChunks[0];
+        //    String[] tmp = new String[msgChunks.length - 1];
+        //    System.arraycopy(msgChunks, 1, tmp, 0, tmp.length);
+        //    msgChunks = tmp;
 
             // dont forget to adjust raw too, so below doesnt mess up
-            raw = raw.substring(extendedCrap.length() + 1); // add one to skip the space
-        }
+        //    raw = raw.substring(extendedCrap.length() + 1); // add one to skip the space
+        //}
 
         String prefix = removeLeadingColon(msgChunks[0]);
 
