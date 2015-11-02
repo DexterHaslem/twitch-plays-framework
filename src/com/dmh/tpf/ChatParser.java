944ffadc1aece48fd600187de29afff72ea0bf45
Dexter Haslem <dexter.haslem@gmail.com>
2015-11-02 05:19:30 -0700

    add handling for twitch extensions IRCv3 stuff

--- src/com/dmh/tpf/ChatParser.java
+++ src/com/dmh/tpf/ChatParser.java
@@ -37,11 +37,25 @@ public class ChatParser {
         if (raw == null || raw.isEmpty())
             return null;
 
+        // oh god, thats all great but we need to handle twitch extensions (IRCv3 stuff)
+        //@color=#0D4200;display-name=TWITCH_UserNaME;emotes=25:0-4,12-16/1902:6-10;subscriber=0;turbo=1;user-type=global_mod :twitch_username!twitch_username@twitch_username.tmi.twitch.tv PRIVMSG #channel :Kappa Keepo Kappa
+        //@color=#0D4200;display-name=TWITCH_UserNaME;emote-sets=0,33,50,237,793,2126,3517,4578,5569,9400,10337,12239;subscriber=1;turbo=1;user-type=staff :tmi.twitch.tv USERSTATE #channel
         String[] msgChunks = raw.split(" ");
 
         // we need at least a prefix and command
         if (msgChunks.length < 2)
             return null;
+
+        // turbo fuck hack: if its IRCv3 tagged, drop that chunk
+        if (msgChunks[0].length() > 1 && msgChunks[0].charAt(0) == '@') {
+            String extendedCrap = msgChunks[0];
+            String[] tmp = new String[msgChunks.length - 1];
+            System.arraycopy(msgChunks, 1, tmp, 0, tmp.length);
+            msgChunks = tmp;
+
+            // dont forget to adjust raw too, so below doesnt mess up
+            raw = raw.substring(extendedCrap.length() + 1); // add one to skip the space
+        }
 
         String prefix = removeLeadingColon(msgChunks[0]);
 
--- src/com/dmh/tpf/test/ChatParserTest.java
+++ src/com/dmh/tpf/test/ChatParserTest.java
@@ -26,5 +26,13 @@ public class ChatParserTest {
         assertEquals(msg.getType(), MessageType.Privmsg);
         assertEquals(msg.getPrivmsgMessage(), "butt farts asdfadsf");
         assertEquals(msg.getPrivmsgChannel(), "#channel");
+
+        // IRCv3 test (ignoring it)
+        testPrivMsg = "@color=#0D4200;display-name=TWITCH_UserNaME;emotes=25:0-4,12-16/1902:6-10;subscriber=0;turbo=1;user-type=global_mod :foo!user@host PRIVMSG #channel :butt farts asdfadsf";
+        msg = ChatParser.parse(testPrivMsg);
+
+        assertEquals(msg.getType(), MessageType.Privmsg);
+        assertEquals(msg.getPrivmsgMessage(), "butt farts asdfadsf");
+        assertEquals(msg.getPrivmsgChannel(), "#channel");
     }
 }
