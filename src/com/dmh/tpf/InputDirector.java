a8d4879054f28578331de24f772f1dd0e67f4cb9
Dexter Haslem <dexter.haslem@gmail.com>
2015-11-01 23:48:59 -0700

    unused imports

--- src/com/dmh/tpf/InputDirector.java
+++ src/com/dmh/tpf/InputDirector.java
@@ -1,14 +1,5 @@
 package com.dmh.tpf;
 
-import com.sun.corba.se.spi.orbutil.fsm.Input;
-import com.sun.jna.Native;
-import com.sun.jna.platform.win32.*;
-import com.sun.jna.ptr.IntByReference;
-import com.sun.jna.win32.W32APIOptions;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public abstract class InputDirector {
     protected String processName;
     public abstract boolean sendKey(int keyCode);
